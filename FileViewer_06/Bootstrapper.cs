46688b9aaa1c6f36e1bd66c17fb7a49a910771b0
nitish-strand <nitishk.pal@strandls.com>
2020-04-28 23:36:05 +0530

    Final working WPF MVVM with DataGrid in Prism(With Basic setup.

---
+++ FileViewer_06/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Unity;
+using System.Windows;
+using Microsoft.Practices.Unity;
+
+namespace FileViewer_06
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        public override void Run(bool runWithDefaultConfiguration)
+        {
+            base.Run(runWithDefaultConfiguration);
+        }
+
+        //create the shell
+        //show the window
+
+        protected override DependencyObject CreateShell()
+        {
+            // make new shell ~ new shell();
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            //Set main window for Prism
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            Type moduleAType = typeof(Module_A.Module);
+            ModuleCatalog.AddModule(new Prism.Modularity.ModuleInfo { ModuleName = moduleAType.Name, ModuleType = moduleAType.AssemblyQualifiedName });
+        }
+
+    }
+}
